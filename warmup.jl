2c8960e6c41f2dab8de93e877a130be207b2b30d
tim-au <99239974+tim-au@users.noreply.github.com>
2022-02-23 17:09:15 +1030

    Update warmup.jl

--- warmup.jl
+++ warmup.jl
@@ -1,13 +1,27 @@
-import BLPDemand
-import Dialysis
+using Plots
+using Unitful
+using LaTeXStrings
+using DataFrames
+using Unitful: N, mm, cm, m, inch, MPa
 
-#try
-#  include(joinpath(pkgdir(BLPDemand), "test", "runtests.jl"))
-#catch e
-#  println("Error in BLPDemand tests, but continuing anyway")
-#end
-try
-  include(joinpath(pkgdir(Dialysis), "test", "runtests.jl"))
-catch e
-  println("Error in Dialysis tests, but continuing anyway")
-end
+θ = LinRange(0, 2π, 100)
+y = sin.(θ)
+
+p = plot(θ, y, 
+	title = L"sin(\theta)",
+	xlabel = L"\theta",
+	ylabel = L"Amplitude",
+	xlim = [0, 2π],
+	ylim = [-1, 1],
+	legend = false
+)
+
+display(p)
+
+F = [1000, 3200, -4000, 4300, -2750, 6630]N
+r = [300mm, 400mm, 10inch, 15cm, 0.188m, 27cm] .|> mm
+
+df = DataFrame(r=r, F=F)
+
+
+
